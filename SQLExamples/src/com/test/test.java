7ab21ed11f9af5776df3ec6bcf2426db86fa7abb
dsin51 <dsinghal3@sapient.com>
2016-02-10 09:27:06 +0530

    commit

---
+++ SQLExamples/src/com/test/test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =9;
+		int b = a/0;
+		System.out.println(b);
+	
+	}
+
+}
